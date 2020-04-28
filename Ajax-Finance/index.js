02e86bba6a1618e5c9288eaad4174448e3aa162a
Emanuele Vernetti <e.vernetti.1033@vallauri.edu>
2020-04-28 12:07:18 +0200

    Commit

--- Ajax-Finance/index.js
+++ Ajax-Finance/index.js
@@ -1,8 +1,11 @@
 $(document).ready(function ()
 {
     let _lst=$("#lstCompanies");
+    let _lstChart=$("#cmbSelectForChart");
     let _txtSearch=$("#txtSearch");
     let _tableQuotes=$("#tableQuotes");
+    let ctx;
+    
     $("body").on("keyup","input",function ()
     {
         showHints($(this).val());
@@ -20,15 +23,54 @@ $(document).ready(function ()
         }
     });
 
+    _lstChart.on("change", function (data)
+    {
+        alert(_lstChart.val());
+        let datasector=inviaRichiesta("GET","http://localhost:3000/SECTOR");
+        datasector.done(function (data)
+        {
+            if(!ctx)
+            {
+                ctx=creaGrafico("http://localhost:3000/chart");
+            }
+            modifica(ctx, data[_lstChart.val()]);
+        });
+    });
+
+    function inviaRichiesta(method, url, parameters = "",async=true)
+    {
+        return $.ajax({
+            type: method,
+            url: url,
+            data: parameters,
+            contentType: "application/x-www-form-urlencoded;charset=utf-8",
+            dataType: "json",
+            timeout: 5000,
+            async:async
+        });
+    }
+
+    function modifica(chart, contenuto)
+    {
+        let dataChart=chart["data"];
+        dataChart["labels"]=[];
+        let dataset=dataChart["datasets"][0];
+        dataset["data"]=[];
+        for (let chiave in contenuto)
+        {
+            dataChart["labels"].push(chiave);
+            dataset["data"].push(contenuto[chiave].replace("%", ""));
+            dataset["backgroundColor"].push("rgb(255, 0, 0)");
+            dataset["borderColor"].push("rgb(255, 0, 0)");
+        }
+        chart.update();
+    }
+
     function showHints(str)
     {
         if(str.length>=2)
         {
-            let n=window.document.getElementById("tableQuotes").rows.length;
-            for(let i=1;i<=n-1;i++)
-            {
-                window.document.getElementById("tableQuotes").deleteRow(1);
-            }
+            _tableQuotes.html("");
             let url="https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+str+"&apikey=CQB6JNQ90AXL7MEF";
             $.getJSON(url,
                 function (data)
@@ -40,11 +82,24 @@ $(document).ready(function ()
                         $.getJSON(url, function (data)
                         {
                             //Caricamento dei dati nella tabella
+
+                            //Codice rimosso a causa del limite delle chiamate imposto da AlphaVantage
                         });
                     }
                 }
             );
         }
+    }
+
+    function creaGrafico(dataChart)
+    {
+        let _data = inviaRichiesta("GET", dataChart,{},false);
+        _data.done(function (data)
+        {
+            return data;
+        });
+        let chart=new Chart($("#myChart"),JSON.parse(_data.responseText));
+        return chart;
     }
 
 
