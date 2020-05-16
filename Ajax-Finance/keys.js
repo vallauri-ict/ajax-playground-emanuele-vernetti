5ea9ab1a3b6e0b7f640575de6082e8dd39703f42
Emanuele Vernetti <e.vernetti.1033@vallauri.edu>
2020-05-16 19:30:05 +0200

    commit

---
+++ Ajax-Finance/keys.js
@@ -0,0 +1,13 @@
+let keys=
+    {"web":
+            {
+                "client_id":"957269115117-tj3vc6kpm67h6iffsnniadd0c5re2v8t.apps.googleusercontent.com",
+                "project_id":"inbound-avatar-275610",
+                "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+                "token_uri":"https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+                "client_secret":"YXdxlYcBAXziwgEqKJCJr6nK",
+                "redirect_uris":["http://127.0.0.1:8080"],
+                "javascript_origins":["http://127.0.0.1:8080"]
+            }
+    }
