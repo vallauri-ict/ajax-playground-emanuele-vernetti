5640bd3b199d31f4d52357f537db36acee4b7a1a
Emanuele Vernetti <58587228+emanuelevernetti@users.noreply.github.com>
2020-05-18 22:54:13 +0200

    backgroundImage update

--- Ajax-Finance/index.js
+++ Ajax-Finance/index.js
@@ -79,6 +79,7 @@ $(document).ready(function ()
     {
         if ($(this).val() != "null")
         {
+            _tbody.html("");
             getGlobalQuotes($(this).val());
         }
         else
